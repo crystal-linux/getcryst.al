49360757728673df21136bd4c76009c1742ca630
kketg <kketg03@gmail.com>
2022-08-30 15:07:53 -0400

    Changed langs, added draft roadmap

--- team.js
+++ team.js
@@ -41,7 +41,6 @@ class Team extends React.Component {
 
     render() {
         //Stores the contributor avatar elements
-        var filteredUsers;
         var avatars;
         if (this.state.fetched) {
             avatars = this.state.users.map((user) => (
@@ -70,113 +69,6 @@ class Team extends React.Component {
     }
 }
 
-class Languages extends React.Component {
-    constructor(props) {
-        super(props);
-        //Tracks whether json data has been fetched from url
-        this.state = { fetched: false, langs: [] };
-        this.getLangData();
-        this.getLangData();
-    }
-
-    getLangData() {
-        fetch("https://api.github.com/repos/crystal-linux/amethyst/languages", {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-        })
-            .then((response) => response.json())
-            .then((responseData) => {
-                for (let key in Object.keys(responseData)) {
-                    let k = Object.keys(responseData)[key];
-                    this.state.langs.push(k);
-                }
-            })
-            .catch(error => console.warn(error));
-        fetch("https://api.github.com/repos/crystal-linux/jade/languages", {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-        })
-            .then((response) => response.json())
-            .then((responseData) => {
-                for (let key in Object.keys(responseData)) {
-                    let k = Object.keys(responseData)[key];
-                    this.state.langs.push(k);
-                }
-            })
-            .catch(error => console.warn(error));
-        fetch("https://api.github.com/repos/crystal-linux/malachite/languages", {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-        })
-            .then((response) => response.json())
-            .then((responseData) => {
-                for (let key in Object.keys(responseData)) {
-                    let k = Object.keys(responseData)[key];
-                    this.state.langs.push(k);
-                }
-            })
-            .catch(error => console.warn(error));
-        fetch("https://api.github.com/repos/crystal-linux/lapis/languages", {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-        })
-            .then((response) => response.json())
-            .then((responseData) => {
-                for (let key in Object.keys(responseData)) {
-                    let k = Object.keys(responseData)[key];
-                    this.state.langs.push(k);
-                }
-            })
-            .catch(error => console.warn(error));
-        fetch("https://api.github.com/repos/crystal-linux/caveman/languages", {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-        })
-            .then((response) => response.json())
-            .then((responseData) => {
-                for (let key in Object.keys(responseData)) {
-                    let k = Object.keys(responseData)[key];
-                    this.state.langs.push(k);
-                }
-            })
-            .catch(error => console.warn(error));
-
-        fetch("https://api.github.com/repos/crystal-linux/site/languages", {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-        })
-            .then((response) => response.json())
-            .then((responseData) => {
-                for (let key in Object.keys(responseData)) {
-                    let k = Object.keys(responseData)[key];
-                    this.state.langs.push(k);
-                }
-                //indicates that data was fetched, then stores only the required data in a list in the object state
-                this.setState({
-                    fetched: true
-                });
-            })
-            .catch(error => console.warn(error));
-    }
-}
 
 const teamContainer = document.querySelector('#team-members');
 const teamRoot = ReactDOM.createRoot(teamContainer);
