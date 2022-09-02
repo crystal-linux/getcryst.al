a628abf03b9796f3bb63be10640cb23e352c0c84
mrshmllow <marshycity@gmail.com>
2022-09-03 09:46:19 +1000

    update /favicons (we are not a PWA or in 2009)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,7 +32,6 @@ const Document = () => (
         sizes="16x16"
         href="/favicons/favicon-16x16.png"
       />
-      <link rel="manifest" href="/favicons/site.webmanifest" />
       <link
         rel="mask-icon"
         href="/favicons/safari-pinned-tab.svg"
