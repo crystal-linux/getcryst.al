fbf0c1c25f9b6bb3c4f9c6de337942d9b2a6c53f
mrshmllow <marshycity@gmail.com>
2022-09-12 12:59:37 +1000

    Improve directory redirects

--- pages/docs/[[...slug]].tsx
+++ pages/docs/[[...slug]].tsx
@@ -2,7 +2,7 @@ import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import { FC } from "react";
 import { resolve } from "path";
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps, Redirect } from "next";
 import { removeExt, walkFiles } from "../../lib/files";
 import { lstat, readFile } from "fs/promises";
 import { readdir } from "fs/promises";
@@ -49,21 +49,23 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   let path = ["_docs", ...slug].join("/");
 
-  try {
-    if ((await lstat(path)).isDirectory()) {
-      const dirents = await readdir(resolve(process.cwd(), path));
-
-      return {
-        props: {},
-        redirect: {
-          destination: `/docs/${slug.join("/")}/${removeExt(dirents.shift()!)}`,
-          permanent: false,
-        },
-      };
-    }
-  } catch {}
+  // try {
+  //   if ((await lstat(path)).isDirectory()) {
+  //     const dirents = await readdir(resolve(process.cwd(), path));
+  //
+  //     return {
+  //       props: {},
+  //       redirect: {
+  //         destination: `/docs/${slug.join("/")}/${removeExt(dirents.shift()!)}`,
+  //         permanent: false,
+  //       },
+  //     };
+  //   }
+  // } catch {}
 
   path = path + ".mdx";
+
+  let redirect: Redirect | null = null;
 
   const walk = async (node: node, dir: string, i = 0) => {
     const dirents = await readdir(resolve(process.cwd(), dir), {
@@ -71,33 +73,47 @@ export const getStaticProps: GetStaticProps = async (context) => {
     });
     for (const dirent of dirents) {
       if (dirent.name.startsWith(".")) {
-        continue
+        continue;
       }
 
       const current = slug[i] === removeExt(dirent.name) && node.current;
       if (dirent.isDirectory()) {
-        node.children.push(
-          await walk(
-            {
-              value: removeExt(dirent.name),
-              children: [],
-              current,
-            },
-            resolve(dir, dirent.name),
-            i + 1
-          )
+        const child = await walk(
+          {
+            value: removeExt(dirent.name),
+            children: [],
+            current,
+          },
+          resolve(dir, dirent.name),
+          i + 1
         );
+        node.children.push(child);
+
+        if (current && i + 1 == slug.length && node.children.length > 0) {
+          // We are a directory AND the path
+          redirect = {
+            permanent: false,
+            destination: `/docs/${slug.join("/")}/${
+              node.children[0].children.shift()!.value
+            }`,
+          };
+        }
       } else {
-        const contents = (await readFile(resolve(dir, dirent.name))).toString();
-        const frontmatter = fm<FrontMatter>(contents);
-        node.children.push({
-          value: removeExt(dirent.name),
-          pretty: frontmatter.attributes.title
-            ? frontmatter.attributes.title
-            : null,
-          children: [],
-          current,
-        });
+        try {
+          const contents = (
+            await readFile(resolve(dir, dirent.name))
+          ).toString();
+
+          const frontmatter = fm<FrontMatter>(contents);
+          node.children.push({
+            value: removeExt(dirent.name),
+            pretty: frontmatter.attributes.title
+              ? frontmatter.attributes.title
+              : null,
+            children: [],
+            current,
+          });
+        } catch (e) {}
       }
     }
     return node;
@@ -111,6 +127,10 @@ export const getStaticProps: GetStaticProps = async (context) => {
     },
     "_docs/"
   );
+
+  if (redirect) {
+    return { props: { tree }, redirect };
+  }
 
   const mdxSource = await serialize(
     (await readFile(resolve(process.cwd(), path))).toString(),
@@ -131,11 +151,11 @@ const DocPage: FC<{ source: MDXRemoteSerializeResult; tree: node }> = ({
 }) => {
   return (
     <>
-      <div className="flex pt-28 gap-4 justify-center md:pt-40">
-        <aside className="flex flex-col w-60">
+      <div className="flex justify-center gap-4 pt-28 md:pt-40">
+        <aside className="flex w-60 flex-col">
           <TreeNode node={tree} path="/docs" />
         </aside>
-        
+
         <DocWrapper>
           {source.frontmatter?.title && <h1>{source.frontmatter.title}</h1>}
           <MDXRemote {...source} />
