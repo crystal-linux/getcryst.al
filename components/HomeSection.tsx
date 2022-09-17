23d5bc9819f036b77d66fd281b97c784bc3025b3
mrshmllow <marshycity@gmail.com>
2022-09-17 16:59:26 +1000

    refractor home section styling

--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -1,8 +1,17 @@
 import { FC, ReactNode } from "react";
 
-const HomeSection: FC<{ children: ReactNode }> = ({ children }) => (
-  <section className="flex flex-col lg:flex-row gap-8 max-w-8xl mx-auto px-8">
-    {children}
+const HomeSection: FC<{ children: ReactNode; className?: string }> = ({
+  children,
+  className,
+}) => (
+  <section
+    className={`py-6 w-full ${
+      className ? className : ""
+    }`}
+  >
+    <div className="flex flex-col px-8 lg:flex-row gap-8 mx-auto max-w-8xl">
+      {children}
+    </div>
   </section>
 );
 
