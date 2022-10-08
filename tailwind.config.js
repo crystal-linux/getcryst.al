3e33d4601c340a7a3b8a63106f84c2b3a6c20a11
mrshmllow <marshycity@gmail.com>
2022-10-08 16:53:43 +1100

    update tailwind prose colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,48 @@ module.exports = {
       fontFamily: {
         "fira-code": ["Fira Code", ...defaultTheme.fontFamily.mono],
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            // I HATE THIS
+            // I have to pass these manualyl because tailwind was not
+            // working with theme() & `ctp`
+            "--tw-prose-body": "#4c4f69",
+            "--tw-prose-headings": "#4c4f69",
+            // "--tw-prose-lead": "",
+            "--tw-prose-links": "#4c4f69",
+            // "--tw-prose-bold": "",
+            "--tw-prose-counters": "#7c7f93",
+            "--tw-prose-bullets": "#7c7f93",
+            "--tw-prose-hr": "#4c4f69",
+            "--tw-prose-quotes": "#4c4f69",
+            "--tw-prose-quote-borders": "#9ca0b0",
+            // "--tw-prose-captions": "",
+            "--tw-prose-code": "#4c4f69",
+            // "--tw-prose-pre-code": "",
+            // "--tw-prose-pre-bg": "",
+            "--tw-prose-th-borders": "#9ca0b0",
+            "--tw-prose-td-borders": "#9ca0b0",
+
+            '--tw-prose-invert-body': "#cdd6f4",
+            '--tw-prose-invert-headings': "#cdd6f4",
+            // '--tw-prose-invert-lead': "",
+            '--tw-prose-invert-links': "#cdd6f4",
+            // '--tw-prose-invert-bold': "",
+            '--tw-prose-invert-counters': "#7c7f93",
+            '--tw-prose-invert-bullets': "#7c7f93",
+            '--tw-prose-invert-hr': "#cdd6f4",
+            '--tw-prose-invert-quotes': "#cdd6f4",
+            '--tw-prose-invert-quote-borders': "#6c7086",
+            // '--tw-prose-invert-captions': "",
+            '--tw-prose-invert-code': "#cdd6f4",
+            // '--tw-prose-invert-pre-code': "",
+            // '--tw-prose-invert-pre-bg': "",
+            '--tw-prose-invert-th-borders': "#6c7086",
+            '--tw-prose-invert-td-borders': "#6c7086",
+          },
+        },
+      },
     },
   },
   plugins: [
