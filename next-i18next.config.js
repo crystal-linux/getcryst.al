2ce621bca73db8f8933ff0bf21d40b7868073d16
guineu <guineuu@pm.me>
2022-10-11 09:50:33 +0200

    add spanish to config
    Signed-off-by: guineu <guineuu@pm.me>

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   /** @type {import('next/dist/server/config-shared').I18NConfig} **/
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "de", "fr", "sv", "nl"],
+    locales: ["en", "de", "fr", "sv", "nl", "es"],
   },
 };
